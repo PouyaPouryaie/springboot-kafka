ac001085d99d09c184c1cd90bd2f3a46de211d55
pouya <pouyapouryaie@gmail.com>
2024-09-25 10:14:55 -0300

    improve test container for consumer side and add latch to check message is received or not

--- consumer/src/main/java/ir/bigz/kafka/KafkaMessageListener.java
+++ consumer/src/main/java/ir/bigz/kafka/KafkaMessageListener.java
@@ -7,21 +7,27 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CountDownLatch;
+
 @Service
 public class KafkaMessageListener {
 
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
+    private CountDownLatch latch = new CountDownLatch(1);
+
 //    @KafkaListener(topics = "kafka-spring-topic", groupId = "spring-group-1")
     @KafkaListener(topics = "kafka-spring-topic")
     public void consume(Customer customer) {
         log.info("Consumer consume the customer: {}", customer);
+        latch.countDown();
     }
 
     @KafkaListener(topics = "pouya-topic", groupId = "pouya-group",
             topicPartitions = {@TopicPartition(topic = "pouya-topic", partitions = {"1"})})
     public void consumeSpecificPartition(String message) {
         log.info("Consumer consume the message: {}, partition: 1", message);
+        latch.countDown();
     }
 
 // for testing
@@ -34,4 +40,12 @@ public class KafkaMessageListener {
 //    public void consumeThree(String message) {
 //        log.info("Consumer 3 consume the message: {}", message);
 //    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    public void resetLatch() {
+        latch = new CountDownLatch(1);
+    }
 }
--- consumer/src/test/java/ir/bigz/kafka/KafkaConsumerTestConfig.java
+++ consumer/src/test/java/ir/bigz/kafka/KafkaConsumerTestConfig.java
@@ -1,7 +1,6 @@
-/*
 package ir.bigz.kafka;
 
-import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -28,40 +27,51 @@ public class KafkaConsumerTestConfig {
     @Autowired
     KafkaContainer kafkaContainer;
 
-    @Bean
-    public Map<String, Object> kafkaConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "ir.bigz.kafka.dto");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "spring-group");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-        return props;
+    Map<String, Object> consumerConfigMap = new HashMap<>();
+    Map<String, Object> producerConfigMap = new HashMap<>();
+
+
+    public void kafkaProducerConfig() {
+        producerConfigMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
+        producerConfigMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerConfigMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+    }
+
+    public void kafkaConsumerConfig() {
+        consumerConfigMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
+        consumerConfigMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerConfigMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumerConfigMap.put(JsonDeserializer.TRUSTED_PACKAGES, "ir.bigz.kafka.dto");
+        consumerConfigMap.put(ConsumerConfig.GROUP_ID_CONFIG, "spring-group");
+        consumerConfigMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     }
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(kafkaConfig());
+        kafkaProducerConfig();
+        return new DefaultKafkaProducerFactory<>(producerConfigMap);
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(kafkaConfig());
+        kafkaConsumerConfig();
+        return new DefaultKafkaConsumerFactory<>(consumerConfigMap);
     }
 
     @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory(ConsumerFactory consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(consumerFactory);
         return factory;
     }
+
+    @Bean
+    public NewTopic createTopicWithNewTopic() {
+        return new NewTopic("pouya-topic", 3, (short) 1);
+    }
 }
-*/
