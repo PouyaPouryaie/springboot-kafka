7804d62660bb80a9b68a39494fc83b050962f141
pouya <pouyapouryaie@gmail.com>
2024-09-24 22:48:38 -0300

    implement test container for consumer side
    Note:
    spring boot connect to org.testcontainers.containers.KafkaContainer, which is deprecated

--- consumer/src/test/java/ir/bigz/kafka/ConsumerApplicationTests.java
+++ consumer/src/test/java/ir/bigz/kafka/ConsumerApplicationTests.java
@@ -1,13 +1,51 @@
 package ir.bigz.kafka;
 
+import ir.bigz.kafka.dto.Customer;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
+import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
+@Testcontainers
 class ConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    Logger log = LoggerFactory.getLogger(ConsumerApplicationTests.class);
 
+    @Container
+    public static KafkaContainer kafkaContainer = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.4.0")
+                    .asCompatibleSubstituteFor("apache/kafka"))
+            .withKraft();
+
+    @DynamicPropertySource
+    static void overridePropertiesInternal(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Test
+    public void testConsumeEvent() {
+        log.info("test consume Event execution started ....");
+        Customer customer = new Customer(-1, "test user", "test@user.com");
+        kafkaTemplate.send("kafka-spring-topic", customer);
+        log.info("test consume Event execution ended ....");
+        Awaitility.await().pollInterval(Duration.ofSeconds(3)).atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+
+        });
+    }
 }
--- consumer/src/test/java/ir/bigz/kafka/KafkaConsumerTestConfig.java
+++ consumer/src/test/java/ir/bigz/kafka/KafkaConsumerTestConfig.java
@@ -1,38 +1,60 @@
+/*
 package ir.bigz.kafka;
 
+import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.testcontainers.containers.KafkaContainer;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
-@Profile("production")
-public class KafkaConsumerConfig {
+@Configuration(proxyBeanMethods = false)
+@Profile("test")
+public class KafkaConsumerTestConfig {
+
+    @Autowired
+    KafkaContainer kafkaContainer;
 
     @Bean
-    public Map<String, Object> consumerConfigs() {
+    public Map<String, Object> kafkaConfig() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.21.0.2:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "ir.bigz.kafka.dto");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "spring-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         return props;
     }
 
     @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(kafkaConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+        return new DefaultKafkaConsumerFactory<>(kafkaConfig());
     }
 
     @Bean
@@ -41,5 +63,5 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
-
 }
+*/
